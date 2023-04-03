ddf19dde3f9d0ee773fc4f10c3465de45d5e2995
JuliaGomozova <sunrain@yandex.ru>
2023-04-03 14:24:22 +0300

    add task1 in hw2

---
+++ HW2/task1.java
@@ -0,0 +1,26 @@
+package Java_start.HW2;
+
+public class task1{
+
+// Вам дается строка S и целочисленный массив индексов int index[s.length].
+// Напишите программу, которая перетасует символы в S таким образом,
+// что символ c i-й позиции переместится на индекс index[i] в результирующей строке.
+//
+// Пример: s = “cba”, index = [3,2,1] result “abc”
+public static void main(String[] args) {
+    String s = "cba";
+    int[] index = {3,2,1};
+    System.out.println(shuffle(s, index));
+}
+
+public static String shuffle(final String s, final int[] index){
+    char[] charArray = new char[s.length()];
+    
+    for(int i = 0; i < s.length(); i ++){
+
+        charArray[index[i]-1] = s.charAt(i);
+    }
+    String result = new String(charArray);
+    return result;
+}
+}
